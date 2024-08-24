d431e7d674dc930bc0e47e13ef9d2ee1389fae23
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-08-24 23:06:56 +0500

    update

--- src/Components/Location/Location.jsx
+++ src/Components/Location/Location.jsx
@@ -12,15 +12,14 @@ const Location = () => {
             <p className="location_text">
               Zamin — bu nafaqat zamonaviy xonadonlar, balki shahar yuragida joylashgan qulaylik va imkoniyatlar markazi. Loyiha sizning har bir kuningizni yengillashtirish va yanada boyitish uchun moʻljallangan.            </p>
             <p className="location_text">
-            Majmuamiz shahar markaziga yaqin joylashgan boʻlib, bu sizning barcha muhim maskanlarga oson va tez yetib borishingizni taʼminlaydi. Ijtimoiy infratuzilma, ish joylari, savdo markazlari va koʻngilochar maskanlar — barchasi sizning atrofingizda!            </p>
+              Majmuamiz shahar markaziga yaqin joylashgan boʻlib, bu sizning barcha muhim maskanlarga oson va tez yetib borishingizni taʼminlaydi. Ijtimoiy infratuzilma, ish joylari, savdo markazlari va koʻngilochar maskanlar — barchasi sizning atrofingizda!            </p>
           </div>
           <div className="location_card_left" data-aos="fade-left" data-aos-offset="-200">
             <img src={universal} alt="" className="location_img" />
           </div>
           <div className="location_card2" data-aos="fade-right" data-aos-offset="-200">
             <p className="location_text2">
-              Central Avenue — Rivojlangan shahar infratuzilmasi qulayliklari va toza
-              havoni bir yerda jamlagan turar-joy majmuasi.
+              Zamin Urgut — Sizning farovon va qulay hayotingiz uchun mukammal joy. Shahar markazidagi qulay joylashuv va barcha zarur qulayliklarni bir joyda mujassam etgan majmuamizda yangi hayotingizni boshlang!"
             </p>
           </div>
         </div>
